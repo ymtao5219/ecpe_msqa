4e0495d23c32cdc10a954402b8e1177307e28fb6
Zhenyu Lei <zhenyulei@umass.edu>
2023-05-12 03:39:59 -0400

    achieve ~3% precision...

--- task1/ECPE-MLL-Pytorch/loss.py
+++ task1/ECPE-MLL-Pytorch/loss.py
@@ -27,7 +27,7 @@ def labelTransform(doc_couples_b):
             y_e_isml[i][emo-1][0] = 1 # the True prob is col 0 and the False prob is col 1
             y_c_isml[i][cau-1][0] = 1
 
-    # modify the ground truth y_isml, add adj_param term here
+    # modify the ground truth y_isml, add * adj_param term here
     y_e_isml[:,:,1] = (1 - y_e_isml[:,:,0])
     y_c_isml[:,:,1] = (1 - y_c_isml[:,:,0])
     y_e_isml[:,:,0] = y_e_isml[:,:,0] * adj_param
@@ -58,18 +58,16 @@ def loss_calc(y_e_list,y_c_list,doc_couples_b,cml_scores,eml_scores,slidingmask)
         # print(y_e_isml.shape,y_e_list[n].shape)
         loss_isml += -torch.sum(torch.mul(y_e_isml,torch.log(y_e_list[n])))\
                         -torch.sum(torch.mul(y_c_isml,torch.log(y_c_list[n])))
-        # loss_isml += -torch.sum(torch.mul(y_e_isml,torch.log(y_e_list[n])) + torch.mul(1-y_e_isml,torch.log(1-y_e_list[n])) / adj_param )\
-        #                 -torch.sum(torch.mul(y_c_isml,torch.log(y_c_list[n])) + torch.mul(1-y_c_isml,torch.log(1-y_c_list[n])) / adj_param )
-    loss_isml /= (torch.numel(y_e_isml[0]) + torch.numel(y_c_isml[0]))
+    loss_isml /= (D*4*N)
     
     cml_out_beforemask = torch.div(1,1+torch.exp(cml_scores))
     eml_out_beforemask = torch.div(1,1+torch.exp(eml_scores))
     loss_cmll = -torch.sum(torch.mul(slidingmask,(torch.mul(y_cml_pairs,torch.log(cml_out_beforemask)) * adj_param\
                                     +torch.mul(1-y_cml_pairs,torch.log(1-cml_out_beforemask)))))\
-                                    / torch.sum(slidingmask[0])             # modify back to "* adj_param"
+                                    / (D*(2*configs.window_size+1))                 # modify back to "* adj_param"
     loss_emll = -torch.sum(torch.mul(slidingmask,(torch.mul(y_eml_pairs,torch.log(eml_out_beforemask)) * adj_param\
                                     +torch.mul(1-y_eml_pairs,torch.log(1-eml_out_beforemask)))))\
-                                    / torch.sum(slidingmask[0])             # modify back to "* adj_param"
+                                    / (D*(2*configs.window_size+1))                 # modify back to "* adj_param"
     
     with torch.no_grad():
         cml_out = torch.mul(slidingmask, cml_out_beforemask)
