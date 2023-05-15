fix bugs in y_e_list; move loss calculation to GPU

task1/ECPE-MLL-Pytorch/model.py
@@ -158,10 +158,10 @@ class ISMLBlock(nn.Module):
         self.fc_eml = nn.Linear(hidden_size*2,D)
 
     def forward(self, s1, y_mask):
-        # scores = None
         self.y_e_list = []
         self.y_c_list = []
-        # s_tmp = s1
+
+        
         
         s_tmp = s1.to(next(self.parameters()).device)  # move s1 to the same device as the module parameters
         
@@ -176,11 +176,15 @@ class ISMLBlock(nn.Module):
             fc_c.to(s1.device)
         self.fc_cml.to(s1.device)
         self.fc_eml.to(s1.device)
-        mask = input_padding(y_mask)
+
+        with torch.no_grad():
+            mask = input_padding(y_mask)
         
         for n in range(self.N):
             # print(self.bilstm_e_list[n](s_tmp))
             e_lstm_out,_ = self.bilstm_e_list[n](s_tmp)
+            # print(e_lstm_out.get_device())
+            # ipdb.set_trace()
             y_e = nn.functional.softmax(self.fc_e_list[n](e_lstm_out),dim=2)
             # mask_expanded = mask.expand_as(y_e)
             # y_e = y_e * mask_expanded
@@ -196,14 +200,20 @@ class ISMLBlock(nn.Module):
 
         e_lstm_out = self.activation(e_lstm_out)
         c_lstm_out = self.activation(c_lstm_out)
+        # print(e_lstm_out.get_device())
+        # ipdb.set_trace()
         
         
-        e_lstm_out = e_lstm_out * mask
-        c_lstm_out = c_lstm_out * mask
+        # e_lstm_out = e_lstm_out * mask
+        # c_lstm_out = c_lstm_out * mask
         
-        mask = mask * mask.transpose(-1, -2) 
-        cml_scores = self.fc_cml(e_lstm_out) * mask
-        eml_scores = self.fc_eml(c_lstm_out) * mask
+        # mask = mask * mask.transpose(-1, -2) 
+        # cml_scores = self.fc_cml(e_lstm_out) * mask
+        # eml_scores = self.fc_eml(c_lstm_out) * mask
+        cml_scores = self.fc_cml(e_lstm_out)
+        eml_scores = self.fc_eml(c_lstm_out)
+
+        # print(cml_scores.get_device())
         # ipdb.set_trace()
         
         return self.y_e_list,self.y_c_list,s_tmp,cml_scores,eml_scores
@@ -259,6 +269,6 @@ def slidingmask_gen(D, W, batch_size, device):
     slidingmask = torch.triu(slidingmask,diagonal=-W)  
     slidingmask = torch.tril(slidingmask,diagonal=W) 
     slidingmask = slidingmask.repeat(batch_size,1,1)
-    slidingmask.to(device=device)
+    slidingmask = slidingmask.to(device=device)
     # print(slidingmask)
     return slidingmask

task1/ECPE-MLL-Pytorch/train.py
@@ -51,6 +51,8 @@ def train_loop(configs, model, train_loader):
                                         W=configs.window_size, 
                                         batch_size=configs.batch_size, 
                                         device=device)
+        # print(sliding_mask.get_device())
+        # ipdb.set_trace()
 
     for train_step, batch in enumerate(train_loader, 1):
         with torch.no_grad():
@@ -62,16 +64,25 @@ def train_loop(configs, model, train_loader):
             bert_segment_b = bert_segment_b.to(device=device)
             bert_masks_b = bert_masks_b.to(device=device)
             bert_clause_b = bert_clause_b.to(device=device)
+
+            
         
         y_e_list, y_c_list, s_final, cml_scores, eml_scores = model(bert_token_b, bert_segment_b, bert_masks_b, bert_clause_b, y_mask_b)
+
+        # print(y_e_list[0].get_device())
+        # print(y_e_list[0].shape)
+        # print(y_e_list[0])
         
         # ipdb.set_trace()
-        loss_total,cml_out,eml_out = loss_calc(y_e_list[0].cpu(),
-                                                y_c_list[0].cpu(),
+        # print(cml_scores.get_device())
+        # print(sliding_mask.get_device())
+        # ipdb.set_trace()
+        loss_total,cml_out,eml_out = loss_calc(y_e_list,
+                                                y_c_list,
                                                 doc_couples_b,
-                                                cml_scores.cpu(),
-                                                eml_scores.cpu(),
-                                                sliding_mask.cpu())
+                                                cml_scores,
+                                                eml_scores,
+                                                sliding_mask)
         with torch.no_grad():
             res = inference(cml_out, eml_out, y_mask_b, mode='logic_and')
             # todo: calculate metrics
@@ -117,12 +128,12 @@ def eval_loop(configs, model, val_loader):
             
             y_e_list, y_c_list, s_final, cml_scores, eml_scores = model(bert_token_b, bert_segment_b, bert_masks_b, bert_clause_b, y_mask_b)
             
-            loss_total,cml_out,eml_out = loss_calc(y_e_list[0].cpu(),
-                                                    y_c_list[0].cpu(),
+            loss_total,cml_out,eml_out = loss_calc(y_e_list,
+                                                    y_c_list,
                                                     doc_couples_b,
-                                                    cml_scores.cpu(),
-                                                    eml_scores.cpu(),
-                                                    sliding_mask.cpu())
+                                                    cml_scores,
+                                                    eml_scores,
+                                                    sliding_mask)
             running_loss += loss_total.item()
             res = inference(cml_out, eml_out, y_mask_b, mode='logic_and')
             # todo: calculate metrics
@@ -150,7 +161,7 @@ def top_k_values(tensor, k=5):
 
     return top_values
 
-def inference(cml_out, eml_out, y_mask_b,mode='avg'):
+def inference(cml_out, eml_out, y_mask_b,mode='avg',topk=False):
     eml_out_T = torch.permute(eml_out, (0,2,1))
     # todo: topk
 