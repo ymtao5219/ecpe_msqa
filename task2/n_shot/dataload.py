2656651dded2c4eb602ea23450d7fb65a52bde11
Xiaocheng Zhang <59710148+Xiaocheng-Zhang@users.noreply.github.com>
2023-05-18 00:03:32 -0400

    added comment

--- task2/n_shot/dataload.py
+++ task2/n_shot/dataload.py
@@ -2,11 +2,13 @@ import pandas as pd
 import os
 
 def read_file_under_paths(path, end='.json'):
+    '''read data file name into list'''
     text_files = [f for f in os.listdir(path)]
     result = [f for f in text_files if f.endswith(end)]
     return result
 
 def num_seq(x):
+    '''helper used to sort'''
     offset = 4
     try:
        return int(x[offset:offset+2])
@@ -14,6 +16,7 @@ def num_seq(x):
        return int(x[offset])
 
 def load_data(file_names, path):
+    '''load the data from file name to pandas df'''
     result = []
     for f in sorted(file_names, key=num_seq):
         fpath = os.path.join(path, f)
@@ -24,7 +27,7 @@ def load_data(file_names, path):
 
 
 def parse_data(data, k=None):
-    '''k passages'''
+    '''parse k passages'''
     if k:
         data = data[:k]
     # result = [{'pair': e[1].pairs[0], 'clauses': e[1].clauses} for e in data.iterrows()]
